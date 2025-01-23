03a57061437bf411a044c2d1694886cceb4ae181
Amen Allah Hemdani <87644616+AmenAllahHemdani@users.noreply.github.com>
2025-01-23 09:55:13 +0100

    Create 23-01-2025.cpp

---
+++ solutions/AmenAllahHemdani/attempts/23-01-2025.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int s = 0; 
+
+    void path(int m,int n){
+        if(m==1 && n==1){
+            s++;
+            return;
+        }else if(m==1){
+            path(m,n-1);
+        }else if(n==1) {
+            path(m-1,n);
+        }else{
+            path(m,n-1);
+            path(m-1,n);
+        }
+    }
+
+    int uniquePaths(int m, int n) {
+        path(m,n);
+        return s;
+    }
+};
